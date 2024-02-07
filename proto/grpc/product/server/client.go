9388334ca100b4c8676a0ce03ecc12ec0d83d494
amanuel <amanuelwakshum2020@gmail.com>
2024-02-07 16:59:27 +0300

    added server stearming rpc

--- proto/grpc/product/server/client.go
+++ proto/grpc/product/server/client.go
@@ -1,38 +1,55 @@
-// client/main.go
 package main
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"log"
-	"google.golang.org/grpc"
+
 	pb "github.com/VENOLD/grpc/grpc/product"
+	"google.golang.org/grpc"
 )
 
 func main() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(":50051", grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("could not connect: %v", err)
-	}
-	defer conn.Close()
+    // Set up a connection to the server.
+    conn, err := grpc.Dial(":50051", grpc.WithInsecure())
+    if err != nil {
+        log.Fatalf("could not connect: %v", err)
+    }
+    defer conn.Close()
 
-	// Create a gRPC client
-	client := pb.NewProductClient(conn)
+    // Create a gRPC client
+    client := pb.NewProductClient(conn)
 
-	// Create a sample ProductRequest
-	request := &pb.ProductRequest{
-		ProductName:        "SampleProduct",
-		ProductDescription: "This is a sample product",
-		ProductPrice:       "19.99",
-		// Add any other fields as needed
-	}
+    // Create a stream to the server
+    stream, err := client.AddProducts(context.Background())
+    if err != nil {
+        log.Fatalf("Error calling AddProducts: %v", err)
+    }
 
-	// Call the AddProduct method
-	response, err := client.AddProduct(context.Background(), request)
-	if err != nil {
-		log.Fatalf("could not add product: %v", err)
-	}
+    // Send requests to the server
+    requests := []*pb.ProductRequest{
+        {ProductName: "SampleProduct1"},
+        {ProductName: "SampleProduct2"},
+        // Add more products as needed
+    }
 
-	// Print the response
-	log.Printf("Server Response: %s", response.Result)
+    for _, req := range requests {
+        if err := stream.Send(req); err != nil {
+            log.Fatalf("%v.Send(%v) = %v", stream, req, err)
+        }
+    }
+    stream.CloseSend()
+
+    // Receive responses from the server
+    for {
+        resp, err := stream.Recv()
+        if err == io.EOF {
+            break
+        }
+        if err != nil {
+            log.Fatalf("%v.Recv() got error %v, want %v", stream, err, nil)
+        }
+        fmt.Printf("Received response: %s\n", resp.Result)
+    }
 }
